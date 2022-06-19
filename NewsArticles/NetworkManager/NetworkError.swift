ecfe879b374a9b0e3d6a619e2c7d91ba5f4c70cb
AdnanJoraid <Adnanjoraidca@gmail.com>
2022-06-19 12:44:44 -0400

    Implemented NetworkError enum to display specific errors when dealing with completion closure in network manager

---
+++ NewsArticles/NetworkManager/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  NewsArticles
+//
+//  Created by Adnan Joraid on 2022-06-19.
+//
+
+import Foundation
+
+enum NetworkError: Error{
+    case serverError
+    case decodingError
+}
