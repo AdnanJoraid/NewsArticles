9e38ff1748d5685c40656a8778bca2c05d3187bb
AdnanJoraid <Adnanjoraidca@gmail.com>
2022-06-19 10:55:21 -0400

    Initial Commit

---
+++ NewsArticles/NewsArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsArticlesApp.swift
+//  NewsArticles
+//
+//  Created by Adnan Joraid on 2022-06-19.
+//
+
+import SwiftUI
+
+@main
+struct NewsArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
