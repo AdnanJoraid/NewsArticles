f3ed1d31d65e6012d89f23514477463c1a8aa6ad
AdnanJoraid <Adnanjoraidca@gmail.com>
2022-06-19 11:02:31 -0400

    Implemented article model

---
+++ NewsArticles/Model/Article.swift
@@ -0,0 +1,20 @@
+//
+//  Article.swift
+//  NewsArticles
+//
+//  Created by Adnan Joraid on 2022-06-19.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    let author: String?
+    let title: String?
+    let description: String?
+    let urlToImage: String?
+    let content: String?
+}
+
+
+
+
---
+++ NewsArticles/Model/Articles.swift
@@ -0,0 +1,12 @@
+//
+//  Articles.swift
+//  NewsArticles
+//
+//  Created by Adnan Joraid on 2022-06-19.
+//
+
+import Foundation
+
+struct Articles: Decodable {
+    let articles: [Article]
+}
